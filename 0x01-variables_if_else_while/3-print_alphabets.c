e600d864f32e73b35f3ed1a65f42016bb27a1cef
S1ad <skowza09@gmail.com>
2023-03-29 09:36:00 -0700

    alphABETS

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: always 0
+ *
+ */
+
+int main(void)
+{
+	int n = 97;
+	int l = 65;
+
+	while (n >= 97)
+	{
+		putchar (n);
+	}
+	while (l >= 65)
+	{
+		putchar (L);
+	}
+	putchar ('\n');
+	return (0);
+}
+
