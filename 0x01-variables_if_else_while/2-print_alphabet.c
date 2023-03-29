f68f57fa1ffcef4ca99656bc9e6e73b5e194ac0a
S1ad <skowza09@gmail.com>
2023-03-29 09:23:15 -0700

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * * main - entry point
+ * * Return: always 0
+ */
+
+int main(void)
+{
+		int n = 97;
+
+		while (n <= 122)
+
+					{
+		putchar(n);
+		n++;
+					}
+
+		putchar('\n');
+		return (0);
+}
