b5567d8685b7cc2223362aded08945fc1a94954a
S1ad <skowza09@gmail.com>
2023-03-29 10:01:38 -0700

    ALPHabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,13 +11,15 @@ int main(void)
 	int n = 97;
 	int m = 65;
 
-	while (n <= 97)
+	while (n <= 122)
 	{
 		putchar (n);
+		n++;
 	}
-	while (m <= 65)
+	while (m <= 90)
 	{
 		putchar (m);
+		m++;
 	}
 	putchar ('\n');
 	return (0);
