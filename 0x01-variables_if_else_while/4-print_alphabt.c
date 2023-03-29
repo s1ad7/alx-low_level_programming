b2b6c96500bd184f97b71518625f1ead76755801
S1ad <skowza09@gmail.com>
2023-03-29 10:27:51 -0700

    4 aplhabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 97)
+
+	{
+		if (n == 101 || n == 113)
+		{
+			n++;
+			continue;
+		}
+	}
+		putchar (n);
+		n++;
+}
+
