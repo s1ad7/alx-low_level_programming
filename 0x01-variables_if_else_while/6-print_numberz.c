9df21b1ea84632ab4f09feb1280725425156dc6f
S1ad <skowza09@gmail.com>
2023-03-30 13:29:46 -0700

    Print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/*
+ * main - Entry point
+ * Return: always 0
+ *
+ */
+
+int main(void)
+{
+
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n + 48);
+	}
+	putchar("\n");
+
+
+	return (0);
+}
