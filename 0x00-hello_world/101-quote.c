d396656d59501ad6de436839992f77b785f54c5e
s1ad7 <skowza09@gmail.com>
2023-05-19 06:52:13 -0700

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,8 @@
 
 int main(void)
 {
-	    char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	        write(STDERR_FILENO, msg, 58);
-		    return (1);
+    char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    write(STDERR_FILENO, msg, 59);
+    return 1;
 }
+
